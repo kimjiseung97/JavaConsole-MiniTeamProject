8699938d75dc29c0b19007e8b3d934cad0b86c8d
kimjieung97 <dickseung23@naver.com>
2023-04-11 21:02:47 +0900

    westernfood수정 및 추가

--- myrecipe/Westernfood.java
+++ myrecipe/Westernfood.java
@@ -2,4 +2,15 @@ package myrecipe;
 
 public class Westernfood extends Food {
 
+
+
+    @Override
+    public String toString() {
+        return "양식 : [" +
+                "음식이름='" + foodname + '\'' +
+                ", 재료=" + material +
+                ", 조리법='" + recipe + '\'' +
+                ", 분류코드='" + Category + '\'' +
+                ']'+"\n";
+    }
 }
