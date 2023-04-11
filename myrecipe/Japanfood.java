2ec602e128bcfe59cbb2a52b9795a44a174fb9e9
kimjieung97 <dickseung23@naver.com>
2023-04-11 21:01:16 +0900

    japanfood 수정및 추가

--- myrecipe/Japanfood.java
+++ myrecipe/Japanfood.java
@@ -2,4 +2,17 @@ package myrecipe;
 
 public class Japanfood extends Food {
 
+    public Japanfood() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "일식 : [" +
+                "음식이름='" + foodname + '\'' +
+                ", 재료=" + material +
+                ", 조리법='" + recipe + '\'' +
+                ", 분류코드='" + Category + '\'' +
+                ']'+"\n";
+    }
 }
