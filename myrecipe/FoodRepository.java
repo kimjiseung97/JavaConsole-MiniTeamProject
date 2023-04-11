a4319b04908d72c3a1612b615aa3be6d8fca1999
kimjieung97 <dickseung23@naver.com>
2023-04-11 20:56:33 +0900

    foodrepository추가

---
+++ myrecipe/FoodRepository.java
@@ -0,0 +1,68 @@
+package myrecipe;
+
+import java.util.ArrayList;
+
+public class FoodRepository {
+
+    public static ArrayList<Food> FoodRecipeList;
+
+    static {
+        FoodRecipeList = new ArrayList<>();
+    }
+
+    public ArrayList<Food> getFoodRecipeList() {
+        return FoodRecipeList;
+    }
+
+    private void setFoodRecipeList(ArrayList<Food> foodRecipeList) {
+        FoodRecipeList = foodRecipeList;
+    }
+
+    public FoodRepository() {
+    }
+
+    private FoodRepository(ArrayList<Food> foodRecipe) {
+        FoodRecipeList = foodRecipe;
+    }
+
+    //각각의 food객체 추가하는 함수
+    public void addnewfoodrecipe(Food foodrecipe){
+        FoodRecipeList.add(foodrecipe);
+    }
+
+    public void showfoodlist(){
+        for (Food food : FoodRecipeList) {
+            System.out.println(food);
+        }
+    }
+
+    //레시피 목록이 비었는지 확인하는 함수
+    public boolean isempy() {
+        if(FoodRecipeList.size()==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //재료이름으로 음식을 찾는 함수
+    public void findbymaterial(String findmaterial) {
+        for (Food food : FoodRecipeList) {
+            if (food.material.contains(findmaterial)){
+                System.out.println(food);
+            }else{
+                System.out.println(findmaterial+"이 들어가는 음식을 찾지못했습니다.");
+            }
+        }
+    }
+
+    public void findbyCategory(String findCategory) {
+        for (Food food : FoodRecipeList) {
+            if(food.Category.contains(findCategory)){
+                System.out.println(food);
+            }else{
+                System.out.println(findCategory+"카테고리 음식을 찾지 못했습니다.");
+            }
+        }
+    }
+}
